ecbb270db55aa9e684c25bb250ac012f0e3d6edb
hafalanqu <pptqattibyanlaren22gmail.com>
2025-11-05 22:45:52 +0700

    upload file

---
+++ script.js
@@ -0,0 +1,5 @@
+// Ini akan menampilkan pesan pop-up saat halaman dimuat
+alert("Selamat datang di website saya!");
+
+// Ini akan mencetak pesan ke konsol browser (bisa dilihat di Inspect Element)
+console.log("Halaman berhasil dimuat.");
